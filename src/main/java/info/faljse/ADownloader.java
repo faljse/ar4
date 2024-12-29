1bbabec17166c6539d683a33519642761f125363
Martin Kunz <martin.michael.kunz@gmail.com>
2024-12-29 20:51:33 +0100

    wwi

--- src/main/java/info/faljse/ADownloader.java
+++ src/main/java/info/faljse/ADownloader.java
@@ -10,6 +10,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -66,7 +67,7 @@ public class ADownloader {
             HttpResponse<String> response =
                     client.send(request, HttpResponse.BodyHandlers.ofString());
             ObjectMapper objectMapper = new ObjectMapper();
-            System.out.printf("Loading \"%s\"\n",bc.getHref());
+            System.out.printf("Loading \"%s\"\n", bc.getHref());
             var broadcastDetail = objectMapper.readValue(response.body(), ResponseDetail.class).getBroadcast();
             executorService.submit(() -> dlStreamItem(broadcastDetail));
         } catch (IOException | InterruptedException e) {
@@ -76,31 +77,45 @@ public class ADownloader {
 
     private void dlStreamItem(Broadcast broadcastDetail) {
         HttpClient client = HttpClient.newHttpClient();
-        String urlt=broadcastDetail.getStreams().getFirst().getUrls().getProgressive();
-        urlt=urlt.substring(0,urlt.indexOf("{"));
-        System.out.printf("Downloading %s\n", urlt);
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(urlt))
-                .build();
-        System.out.println(urlt);
-        String dateStr=broadcastDetail.getStart();
-        dateStr=dateStr.substring(0,dateStr.indexOf("."));
-        String fileName= String.format("%s_%d.mp3", dateStr, broadcastDetail.getId());
-        if (Paths.get(folderName, fileName).toFile().exists()) {
-            System.out.printf("Skip %d\n", broadcastDetail.getId());
-            return;
+        String dateStr = broadcastDetail.getStart();
+        dateStr = dateStr.substring(0, dateStr.indexOf("."));
+        String lastFileURL="";
+        for (int i = 0; i < broadcastDetail.getStreams().size(); i++) {
+            var stream=broadcastDetail.getStreams().get(i);
+            String fileName = String.format("%s_%s_%d.mp3", dateStr, broadcastDetail.getId(), i);
+            if (Paths.get(folderName, fileName).toFile().exists()) {
+                System.out.printf("Skip %d\n", broadcastDetail.getId());
+                return;
+            }
+            var streamURL=stream.getUrls().getProgressive();
+            var fileURL=streamURL.substring(0,streamURL.lastIndexOf(".mp3")+".mp3".length());
+            if(fileURL.equals(lastFileURL)) {
+                System.out.printf("same url: %s\n", fileURL);
+                continue;
+            }
+            lastFileURL=fileURL;
+            dlFile(client, fileURL, fileName);
         }
 
-
-        try (OutputStream jsonOut = new FileOutputStream(Paths.get(folderName, fileName+".json").toFile())) {
+        try (OutputStream jsonOut = new FileOutputStream(Paths.get(folderName, String.format("%s_%s.json", dateStr, broadcastDetail.getId())).toFile())) {
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.writerWithDefaultPrettyPrinter().writeValue(jsonOut, broadcastDetail);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private void dlFile(HttpClient client, String url, String fileName) {
 
-        try (OutputStream outStream = new FileOutputStream(Paths.get(folderName, fileName + ".part").toFile())) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        System.out.println(url);
+
+        System.out.printf("Downloading %s\n", url);
+
+        Path path = Paths.get(folderName, fileName + ".part");
+        try (OutputStream outStream = new FileOutputStream(path.toFile())) {
             HttpResponse<InputStream> response =
                     client.send(request, HttpResponse.BodyHandlers.ofInputStream());
 
@@ -111,10 +126,8 @@ public class ADownloader {
             while ((bytesRead = response.body().read(buffer)) != -1) {
                 outStream.write(buffer, 0, bytesRead);
                 this.bytesLoaded.getAndAdd(bytesRead);
-
                 totalBytesRead += bytesRead;
                 if (totalBytesRead - lastBytesRead > 1024 * 1024) {
-                    // System.out.printf("%d MB%n", totalBytesRead/(1024*1024));
                     lastBytesRead = totalBytesRead;
                 }
             }
@@ -122,10 +135,9 @@ public class ADownloader {
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
-            File fileToMove = Paths.get(folderName, fileName + ".part").toFile();
+            File fileToMove = path.toFile();
             fileToMove.renameTo(Paths.get(folderName, fileName).toFile());
         }
-
 
     }
 
